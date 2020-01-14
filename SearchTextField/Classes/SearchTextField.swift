223ce022a7d6685469f3937c9bdc4901c601b783
Tsimafei Harhun <timgorgun@gmail.com>
2020-01-14 10:52:55 +0300

    Update delay

--- SearchTextField/Classes/SearchTextField.swift
+++ SearchTextField/Classes/SearchTextField.swift
@@ -686,3 +686,23 @@ enum Direction {
     case down
     case up
 }
+
+////////////////////////////////////////////////////////////////////////
+// DelayManager
+
+class DelayManager {
+    
+    var isCancel = false
+    func delayWithSeconds(_ seconds: Double, completion: @escaping () -> ()) {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            if !self.isCancel {
+                completion()
+            }
+        }
+    }
+    
+    deinit {
+        isCancel = true
+    }
+}
